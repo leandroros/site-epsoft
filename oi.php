3948d7b7e325a20be352658b9e1a90518ee56e8d
leandroros <leleoros@gmail.com>
2024-06-05 11:30:15 -0300

    oi.php

--- oi.php
+++ oi.php
@@ -1,4 +1,25 @@
 <?php
+// Recipient
+$to_email = "contato@epsoft.com.br";
 
+// Sender
+$from_email = "contato@epsoft.com.br";
 
-echo "OI";
+// Subject
+$subject = "Test Email";
+
+// Message
+$message = "This is a test email sent from PHP.";
+
+// Headers
+$headers = "From: $from_email\r\n";
+$headers .= "Reply-To: $from_email\r\n";
+$headers .= "Content-type: text/html\r\n";
+
+// Send email
+if (mail($to_email, $subject, $message, $headers)) {
+    echo "Email sent successfully to $to_email";
+} else {
+    echo "Email sending failed...";
+}
+?>
